e4cd62193edf2b4cbe36d6bb2db219c540f5bbde
PeeusD <2014pgmca076@nitjsr.ac.in>
2021-08-14 06:58:37 +0530

    bug fixed-twice execution

--- bot.py
+++ bot.py
@@ -1,22 +1,24 @@
-
-from telegram.ext import Updater
-import schedule
 from ques_sets import schedulling_query, schedulling_query2
-import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from flask import Flask, render_template
 
 
 
-schedule.every().day.at("03:35").do(schedulling_query)    ##### SET 03:35  <--------------  CHANGE HERE FOR DEBUGGING  ------>
-schedule.every().day.at("06:35").do(schedulling_query2 )    ##### SET 06:35  <--------------  CHANGE HERE FOR DEBUGGING  ------>
+sched = BackgroundScheduler()
 
-while True:
-  
-    # Checks whether a scheduled task... 
-    #... is pending to run or not
-    schedule.run_pending()
-    time.sleep(1)            
+# Schedules job_function to be run on every day at 3:35 am
+
+sched.add_job(schedulling_query, 'cron', hour='03', minute='35')   # SET utc 03:35  <--------------  CHANGE HERE FOR DEBUGGING  ------>
+sched.add_job(schedulling_query2, 'cron', hour='06', minute='35')   # SET utc 06:35  <--------------  CHANGE HERE FOR DEBUGGING  ------>
+sched.start()
 
 
-updater = Updater(TOKEN)  
-updater.start_polling()
-updater.idle()
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template ("index.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=False, use_reloader=False)
